b31cc15fec7d3efb067f0b29d6733c0ca8f7d1c0
milos <milos@example.com>
2025-08-20 18:04:36 +0200

    Initial commit: project scaffolding and assets

---
+++ app/companions/new/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Page = () => {
+    return (
+        <div>new companion</div>
+    )
+}
+export default Page
---
+++ app/companions/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Page = () => {
+    return (
+        <div>companion library</div>
+    )
+}
+export default Page
